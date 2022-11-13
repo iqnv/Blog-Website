7fc8080436f464b904c8a83e39dbc40c19459227
unknown <faangshivam123@gmail.com>
2022-11-13 17:56:24 +0530

    initial

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+var pick = "";
 
-const homeStartingContent = "Welcome to my Blog website , it is simple and helpful website which contains Contact link, About us link (top right corner). On this Web app you can write a blog in compose section ,in the compose section you can write your blog and publish it , your blog will be automatically saved in database, And also your blog will be available on home page as well.So enjoy this blogging web appliation focus on having a great blog, focus on producing a blog that’s great your readers blogging is a conversation, not a code, blog is only  interesting a the interest shown others, blogging is just writing — writing using a particularly efficient type  publishing technology,blogging is to writing what extreme sports are to athletics more free form, more accident prone, less formal, more alive, it is, many ways, writing out loud,don’t to plan everything out to the very last detail, a big believer just getting it out there create a minimal viable product or website, launch it, and  feedback.";
+const homeStartingContent =" ";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "If you wanna contact us , You can mail us on maild ID: faangshivam123@gmail.com"
+const contactContent = "If you wanna contact us , You can mail us on maild ID: faangshivam123@gmail.com Or You can contactContent"
 
 const app = express();
 
@@ -19,7 +20,8 @@ app.use(express.static("public"));
 mongoose.connect("mongodb+srv://admin-shivam123:test-123@cluster0.ogzyvyu.mongodb.net/blogDB", {useNewUrlParser: true});
 const postSchema = {
   title: String,
-  content: String
+  content: String,
+  pick: String
 };
 
 const Post = mongoose.model("Post", postSchema);
@@ -41,8 +43,10 @@ app.get("/compose", function(req, res){
 app.post("/compose", function(req, res){
   const post = new Post({
     title: req.body.postTitle,
-    content: req.body.postBody
+    content: req.body.postBody,
+    pick: req.body.filename
   });
+
 
 
   post.save(function(err){
@@ -59,7 +63,8 @@ const requestedPostId = req.params.postId;
   Post.findOne({_id: requestedPostId}, function(err, post){
     res.render("post", {
       title: post.title,
-      content: post.content
+      content: post.content,
+      pick: post.pick
     });
   });
 
